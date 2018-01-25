7253148d5deb5469bd00ec3ec2f5eaee12f2c7b3
Dmitry <dm1.f3d@gmail.com>
2018-01-25 16:04:49 +0300

    Добавить простой список с подгрузкой с сервера

---
+++ eios/eios/Occupation.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace eios
+{
+    public class Occupation : INotifyPropertyChanged
+    {
+        private string id;
+        private string name;
+        private string aud;
+        private string time;
+
+        [JsonProperty("id_occup")]
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                NotifyPropertyChanged("Id");
+            }
+        }
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        [JsonProperty("aud")]
+        public string Aud
+        {
+            get { return aud; }
+            set
+            {
+                aud = value;
+                NotifyPropertyChanged("Aud");
+            }
+        }
+
+        [JsonProperty("time")]
+        public string Time
+        {
+            get { return time; }
+            set
+            {
+                time = value;
+                NotifyPropertyChanged("Time");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- eios/eios/OccupationsPage.xaml.cs
+++ eios/eios/OccupationsPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +18,36 @@ namespace eios
 		public OccupationsPage ()
 		{
 			InitializeComponent ();
+
+            LoadData();
 		}
-	}
+
+        private async void LoadData()
+        {
+            string url = "http://q9875032.beget.tech/hp_api/api.php";
+
+            var login = "test";
+            var password = "test1";
+            var type = "get_info";
+            var id = "1";
+            StringContent stringContent = new StringContent(
+                "{ \"login\": \""+login+"\"," +
+                "  \"password\": \""+password+"\"," +
+                "  \"type\": \"" + type + "\"," +
+                "  \"id_group\": \"" + id + "\" }",
+                UnicodeEncoding.UTF8,
+                "application/json");
+
+            HttpClient client = new HttpClient();
+            var response = await client.PostAsync(url, stringContent);
+            response.EnsureSuccessStatusCode();
+
+            // Десериализация ответа
+            var content = await response.Content.ReadAsStringAsync();
+
+            var ocupations = JsonConvert.DeserializeObject<List<Occupation>>(content);
+
+            listView.ItemsSource = ocupations;
+        }
+    }
 }
