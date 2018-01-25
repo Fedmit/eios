51212df07a882c1d1e0eecd6e1b1d36e1f5def09
Dmitry <dm1.f3d@gmail.com>
2018-01-25 21:49:17 +0300

    Доделать внешний вид списка предметов

---
+++ eios/eios.iOS/CircleViewRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using eios;
+using eios.iOs;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CircleView), typeof(CircleViewRenderer))]
+namespace eios.iOs
+{
+    public class CircleViewRenderer : BoxRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+                return;
+
+            Layer.MasksToBounds = true;
+            Layer.CornerRadius = (float)((CircleView)Element).CornerRadius / 2.0f;
+        }
+
+    }
+}
---
+++ eios/eios/BedgeView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eios
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BedgeView : Grid
+	{
+        public string Text
+        {
+            set
+            {
+                label.Text = value;
+            }
+        }
+
+        public int Size
+        {
+            set
+            {
+                WidthRequest = value;
+                HeightRequest = value;
+                circle.WidthRequest = value;
+                circle.HeightRequest = value;
+            }
+        }
+
+		public BedgeView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
